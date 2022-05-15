8015468480193bed5891625d8b60dccf10c2d142
marinaBergas <marina.bergas@gmail.com>
2022-05-15 14:14:08 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+import {View, StyleSheet} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
